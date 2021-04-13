7cf7854e846fe1736e7cf232e1e8de3bb5080cae
miclau13 <miclau2004@gmail.com>
2021-04-14 01:10:07 +0800

    update create and recovery

--- python_zip_example/test/__main__.py
+++ python_zip_example/test/__main__.py
@@ -29,8 +29,6 @@ def build_arg_parser():
 
     parser.add_argument("-p", "--port", type=int, help=porthelp)
     parser.add_argument("-ts", "--target_size", type=int, help=target_sizehelp)
-    # parser.add_argument("port", type=int, help=porthelp)
-    # parser.add_argument("target_size", type=int, help=target_sizehelp)
 
     return parser
 
@@ -64,22 +62,18 @@ class RepeatedTimer:
         self.event.set()
         self.thread.join()
 
-def check_num_of_segments(host, isMain=True):
+def check_num_of_segments(host, target_size):
     url = 'http://{}/info'.format(host)
     response = urlopen(url)
     string = response.read().decode('utf-8')
     json_obj = json.loads(string)
-    print("json_obj:",json_obj)
     other_gates = json_obj['other_gates']
     numsegments = json_obj['numsegments']
-    print("numsegments:",numsegments)
     gate_num = len(other_gates) + 1
-    segement_size = int (target_size / gate_num)
-    this_gate_segement_size = int (segement_size + target_size % gate_num)
-    print("segement_size:", segement_size)
-    print("this_gate_segement_size:", this_gate_segement_size)
-    gate_segment_size = this_gate_segement_size if isMain else segement_size
-    return (numsegments, gate_segment_size, other_gates)
+    segement_size = 1 if target_size < gate_num else int (target_size / gate_num)
+    # this_gate_segement_size = int (segement_size + target_size % gate_num)
+    # gate_segment_size = this_gate_segement_size if isMain else segement_size
+    return (numsegments, segement_size, other_gates)
 
 
 def spread_segment(host, target_size, other_gates):
@@ -96,20 +90,19 @@ def spread_segment(host, target_size, other_gates):
     params = 'args=-p&args={}&args=-ts&args={}'.format(port, target_size)
     url = 'http://{}/worm_entrance?{}'.format(host, params)
 
-    print('url:{}'.format(url))  
     req = Request(url, data)
     response = urlopen(req)
-    print('response:{}'.format(response))
 
 def spread(host, target_size, other_gates):
-    segements = check_num_of_segments(host)
+    segements = check_num_of_segments(host, target_size)
     numsegments = segements[0]
     this_gate_segement_size = segements[1]
     if numsegments < this_gate_segement_size:
         spread_segment(host, target_size, other_gates)
 
-    for g in range(len(other_gates)):
-        other_segements = check_num_of_segments(other_gates[g], False)
+    num_gates_to_spread = target_size - 1 if target_size - 1 < len(other_gates) else len(other_gates) 
+    for g in range(target_size - 1):
+        other_segements = check_num_of_segments(other_gates[g], target_size)
         other_numsegments = other_segements[0]
         other_gate_segement_size = other_segements[1]
         other_other_gates = other_segements[2]
@@ -123,14 +116,14 @@ if __name__ == '__main__':
     port = args.port
     host = hostname + ":" + str(port)
     target_size = args.target_size
-    print("host:", host)
-    print("target_size:", target_size)
 
-    segements = check_num_of_segments(host)
+    segements = check_num_of_segments(host, target_size)
     numsegments = segements[0]
     gate_segement_size = segements[1]
     other_gates = segements[2]
-    if numsegments <= gate_segement_size:
-        timer = RepeatedTimer(10, spread, host, target_size, other_gates)
+    if target_size == 0:
+        print("Target size is 0, no worm is spawned ")
+    elif numsegments <= gate_segement_size:
+        timer = RepeatedTimer(3, spread, host, target_size, other_gates)
     else:
-        print("Enough number of worms in this portal, %s please die", host)
+        print("Enough number of worms in this portal (%s), no worm is spawned ", host)
